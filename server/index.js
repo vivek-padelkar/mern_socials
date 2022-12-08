3ae119decd4d1444b1e8e247cc4a8ef6437fe8c1
Vivek Padelkar <vivek.padelkar@gmail.com>
2022-12-08 08:47:27 +0530

    added code for mongodb conection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import mongoose from 'mongoose'
+import mongoose, { mongo } from 'mongoose'
 import cors from 'cors'
 import multer from 'multer'
 import morgan from 'morgan'
@@ -7,6 +7,7 @@ import dotenv from 'dotenv'
 import helmet from 'helmet'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import mongoose from 'mongoose'
 
 dotenv.config()
 const app = express()
@@ -17,10 +18,26 @@ const PORT = process.env.PORT || 5003
 
 app.use(express.json())
 app.use(helmet())
+app.use(cors())
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
 app.use(morgan('common'))
 app.use('/assets', express.static(path.join(__dirName, 'public/assets')))
 
-app.listen(process.env.PORT, () => {
-  console.log('Running your socials backend ON port ' + PORT)
+
+const storege = multer.diskStorage({destination: function (req,file,cb){
+  cb(null,'public/assets')
+},
+filename: function (req,file,cb){
+  cb(null,file.originalname)
+},
 })
+
+const upload  = multer({storege})
+
+mongoose.connect(process.env.MONGOURI).then(()=>{
+
+  app.listen(process.env.PORT, () => {
+    console.log('Running your socials backend ON port ' + PORT)
+  })
+})
+
